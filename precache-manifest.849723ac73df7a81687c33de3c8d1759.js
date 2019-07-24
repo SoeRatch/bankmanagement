6d50fa250c0bdf627d2f910879ee17026d73c2af
SoeRatch <surajkoho@gmail.com>
2019-07-24 15:31:42 +0530

    Updates

---
+++ precache-manifest.849723ac73df7a81687c33de3c8d1759.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f5bb46db8deff8d240ecbfc57b0822",
+    "url": "/bankmanagement/index.html"
+  },
+  {
+    "revision": "de680e01fd5da10eeb89",
+    "url": "/bankmanagement/static/css/main.16a6d626.chunk.css"
+  },
+  {
+    "revision": "de7c7fd6279bacbf5023",
+    "url": "/bankmanagement/static/js/2.5408e437.chunk.js"
+  },
+  {
+    "revision": "de680e01fd5da10eeb89",
+    "url": "/bankmanagement/static/js/main.cffc6162.chunk.js"
+  },
+  {
+    "revision": "edfd2b2a637556d77758",
+    "url": "/bankmanagement/static/js/runtime~main.4555ca32.js"
+  }
+]);
